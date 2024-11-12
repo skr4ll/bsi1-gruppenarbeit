e7776b1f7e4d6b2c8d00e2206e0604faf0c65b84
skrall <skrall_pa@protonmail.com>
2024-11-12 14:20:22 +0100

    add filter and adjust html

--- js/philipp-script.js
+++ js/philipp-script.js
@@ -1,15 +1,16 @@
 // Script for the tourdates.html page
 const venue_div = document.getElementById("div_venues");
+const error_div = document.getElementById("div_error");
 
 async function fetchVenues() {
     const response = await fetch('data/venues.json');
-    return await response.json();
+    const data = await response.json();
+    return data.venues;
   }
   
-async function displayVenues() {
-    const data = await fetchVenues();
-
-    data.venues.forEach(venue => {
+async function displayVenues(venues) {
+  venue_div.innerHTML = "";  
+  venues.forEach(venue => {
         let venue_header = document.createElement("h2");
         venue_header.textContent = venue.name;
         venue_div.appendChild(venue_header);
@@ -25,8 +26,23 @@ async function displayVenues() {
         venue_div.appendChild(venue_para);
     });
 }
+fetchVenues().then(displayVenues);
 
-const lol = () => {
-    alert("LOL");
-}
-displayVenues();
+async function filter() {
+  event.preventDefault();
+  let sale_status, sitze_vorhanden, stehen_vorhanden, location= "";
+  location = document.getElementById('location').value;
+  if (document.getElementById('sale-status').checked) {sale_status = "on-sale";}
+  if (document.getElementById('sitze-vorhanden').checked) {sitze_vorhanden = "ja";}
+  if (document.getElementById('stehen-vorhanden').checked){stehen_vorhanden = "ja";}
+  
+  const data = await fetchVenues();
+  const filtered_data = data.filter(venue => 
+    (location ? (venue.kontinent === location || venue.land === location) : true) &&
+    (sale_status ? venue.status === sale_status : true) &&
+    (sitze_vorhanden ? venue.sitzplatz === sitze_vorhanden : true) &&
+    (stehen_vorhanden ? venue.stehplatz === stehen_vorhanden : true)
+  );
+  displayVenues(filtered_data);
+
+  }
